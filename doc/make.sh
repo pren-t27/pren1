8dff020f9596d8948c67fae23324cf7b58aead5a
daniw <dani.winz@bluewin.ch>
2014-10-02 11:38:07 +0200

    Projektanforderungen übernommen

--- doc/make.sh
+++ doc/make.sh
@@ -7,40 +7,37 @@
 homedir=/home/daniw/data/studium/sem5/pren1/doc
 # homedir=.
 
-
-
 cd $homedir/charta
-pwd
 pdflatex charta.tex
 pdflatex charta.tex
 pdflatex charta.tex
 
 cd $homedir/matrix
-pwd
 pdflatex matrix.tex
 pdflatex matrix.tex
 pdflatex matrix.tex
 
 cd $homedir/produktanforderungen
-pwd
 pdflatex produktanforderungen.tex
 pdflatex produktanforderungen.tex
 pdflatex produktanforderungen.tex
 
+cd $homedir/projektanforderungen
+pdflatex projektanforderungen.tex
+pdflatex projektanforderungen.tex
+pdflatex projektanforderungen.tex
+
 cd $homedir/protocol
-pwd
 pdflatex protocol_01.tex
 pdflatex protocol_01.tex
 pdflatex protocol_01.tex
 
 cd $homedir/team
-pwd
 pdflatex team.tex
 pdflatex team.tex
 pdflatex team.tex
 
 cd $homedir/technologierecherche
-pwd
 pdflatex technologierecherche.tex
 pdflatex technologierecherche.tex
 pdflatex technologierecherche.tex
